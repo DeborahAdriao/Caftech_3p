a4cec06279951a9af070f7e18bd468632b5b66ef
mikhelli <raianymikhelli@gmail.com>
2025-11-12 23:56:09 -0300

    Criação da Main e Inicio dos testes

--- src/br/com/caftech/Main.java
+++ src/br/com/caftech/Main.java
@@ -3,10 +3,7 @@ package br.com.caftech;
 public class Main {
 
     public static void main(String[] args) {
-        // 1. Cria o objeto que controla o menu
         MenuAtendimento menu = new MenuAtendimento();
-
-        // 2. Inicia o atendimento
         menu.iniciar();
     }
 }
